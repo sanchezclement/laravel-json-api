9a0633c85d01126d58566730d60e8fdd348f7fcc
Clément SANCHEZ <p.clementsanchez@gmail.com>
2020-02-08 14:27:21 +0000

    Added JsonApiProvider

---
+++ src/Providers/JsonApiProvider.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace JsonApi\Providers;
+
+use App\Console\Commands\DiscoverResources;
+use Carbon\Laravel\ServiceProvider;
+use Illuminate\Http\Resources\Json\Resource;
+
+/**
+ * Class JsonApiProvider
+ * @package JsonApi\Providers
+ */
+class JsonApiProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config' => config_path(),
+            ], 'laravel-json-api-config');
+
+            $this->commands([
+                DiscoverResources::class
+            ]);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Resource::withoutWrapping();
+    }
+}

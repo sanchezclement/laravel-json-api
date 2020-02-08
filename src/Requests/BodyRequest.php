34e369105293ef4fadf827e44ffa227f764fe8e0
Clément SANCHEZ <p.clementsanchez@gmail.com>
2020-02-08 22:50:34 +0000

    Fixed a bug

--- src/Requests/BodyRequest.php
+++ src/Requests/BodyRequest.php
@@ -21,11 +21,24 @@ class BodyRequest extends BaseRequest
     }
 
     /**
-     * @param array $parameters
+     * @param array $query The GET parameters
+     * @param array $request The POST parameters
+     * @param array $attributes The request attributes (parameters parsed from the PATH_INFO, ...)
+     * @param array $cookies The COOKIE parameters
+     * @param array $files The FILES parameters
+     * @param array $server The SERVER parameters
+     * @param string|resource|null $content The raw body data
      */
-    public function __construct(...$parameters)
+    public function __construct(
+        array $query = [],
+        array $request = [],
+        array $attributes = [],
+        array $cookies = [],
+        array $files = [],
+        array $server = [],
+        $content = null)
     {
-        parent::__construct(...$parameters);
+        parent::__construct($query, $request, $attributes, $cookies, $files, $server, $content);
 
         $this->__constructHasInclusion();
         $this->__constructHasRelations();

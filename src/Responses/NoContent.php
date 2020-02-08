f1213e514361a135fdf6997faf7bd2fe968c2330
Clément SANCHEZ <p.clementsanchez@gmail.com>
2020-02-08 23:49:18 +0000

    Fixed a bug

---
+++ src/Responses/NoContent.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace JsonApi\Responses;
+
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Class AcceptedResponse
+ * @package JsonApi\Responses
+ */
+class NoContent extends JsonResponse
+{
+    /**
+     * AcceptedResponse constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(null, 204, [], 0);
+    }
+
+    /**
+     * @return static
+     */
+    public static function make(): self
+    {
+        return new static();
+    }
+}

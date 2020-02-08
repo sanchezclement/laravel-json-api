10b49392136e232bb242d8f1d0ef63fe8e33745c
Clément SANCHEZ <p.clementsanchez@gmail.com>
2020-02-08 22:47:23 +0000

    Fixed a bug

--- src/Requests/BodyRequest.php
+++ src/Requests/BodyRequest.php
@@ -3,11 +3,10 @@ declare(strict_types=1);
 
 namespace JsonApi\Requests;
 
+use Illuminate\Support\Arr;
 use JsonApi\Requests\Traits\HasAttributes;
-use JsonApi\Requests\Traits\HasBody;
 use JsonApi\Requests\Traits\HasInclusion;
 use JsonApi\Requests\Traits\HasRelations;
-use Illuminate\Support\Arr;
 use JsonApi\Resources\ResourceObject;
 
 /**
@@ -16,7 +15,25 @@ use JsonApi\Resources\ResourceObject;
  */
 class BodyRequest extends BaseRequest
 {
-    use HasBody, HasInclusion;
+    use HasInclusion, HasAttributes, HasRelations {
+        HasInclusion::__construct as __constructHasInclusion;
+        HasRelations::__construct as __constructHasRelations;
+    }
+
+    /**
+     * @param array $parameters
+     */
+    public function __construct(...$parameters)
+    {
+        parent::__construct(...$parameters);
+
+        $this->__constructHasInclusion();
+        $this->__constructHasRelations();
+
+        $this->beforeValidation(function () {
+            $this->initializeBody();
+        });
+    }
 
     /**
      * @return ResourceObject
@@ -24,5 +41,29 @@ class BodyRequest extends BaseRequest
     public function makeResource()
     {
         return ResourceObject::make($this->getModel(), $this->getInclusions());
+    }
+
+    private function initializeBody(): void
+    {
+        $rules = [
+            'data' => 'required|array',
+            'data.type' => 'required_with:data,string',
+            'data.attributes' => 'array',
+            'data.relationships' => 'array',
+            'data.relationships.*' => 'array',
+            'data.relationships.*.data' => 'required_with:data.relationships.*|array',
+            'data.relationships.*.data.id' => 'required_with:data.relationships.*',
+            'data.relationships.*.data.type' => 'required_with:data.relationships.*'
+        ];
+
+        foreach ($this->attributesRules() as $key => $attribute) {
+            $rules['data.attributes.' . $key] = $attribute;
+        }
+
+        foreach (Arr::wrap($this->getRequiredRelations()) as $relation) {
+            $rules['data.relationships.' . $relation] = 'required';
+        }
+
+        $this->addRules($rules);
     }
 }

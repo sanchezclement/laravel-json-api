510b7b398d18990663dde96a739a6ca780bf1703
Clément SANCHEZ <p.clementsanchez@gmail.com>
2020-02-09 12:15:48 +0000

    Fixed a bug

--- src/Requests/Traits/HasModel.php
+++ src/Requests/Traits/HasModel.php
@@ -64,6 +64,7 @@ trait HasModel
         throw new NotImplementedFunction();
     }
 
+
     protected final function failedAuthorization()
     {
         abort(403, "The main resource action is not authorized");
